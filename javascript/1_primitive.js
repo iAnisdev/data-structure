679e3ff8f1e18f1de8504f10054e213c412b9794
Anis <me.anis.dev@gmail.com>
2025-12-30 00:07:00 +0000

    Add initial JavaScript file with variable type examples and remove unnecessary .gitkeep

---
+++ javascript/1_primitive.js
@@ -0,0 +1,23 @@
+let age = 21
+console.log(typeof age) // number
+
+let pi = 3.14 // decimal number and integer are both of type 'number' in JavaScript
+console.log(typeof pi) // number
+
+let isStudent = true
+console.log(typeof isStudent) // boolean
+
+let name = "Alice"
+console.log(typeof name) // string
+
+let address = null
+console.log(typeof address) // object
+
+let phoneNumber = undefined
+console.log(typeof phoneNumber) // undefined
+
+let bigNumber = 9007199254740991n
+console.log(typeof bigNumber) // bigint
+
+let symbol = Symbol("id")
+console.log(typeof symbol) // symbol
